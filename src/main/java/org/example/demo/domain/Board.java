533a9eb3b590d45715741a132cbd8dad1a2136b0
unknown <gusdudco6@naver.com>
2024-04-26 10:21:21 +0900

    🧐 Entity Settings
    Entity Settings

--- src/main/java/org/example/demo/domain/Board.java
+++ src/main/java/org/example/demo/domain/Board.java
@@ -1,19 +1,23 @@
 package org.example.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity     //엔티티 선언
+@Getter
+@Builder    //Setter 대신 Builder로 넣고 싶다.
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Board extends BaseEntity{
     @Id     //pk 선언
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
-
+    @Column(nullable = false,length = 500)
     private String title;
-
+    @Column(length = 2000, nullable = false)
     private String content;
 
+    @Column(length = 50, nullable = false)
     private String writer;
 }
