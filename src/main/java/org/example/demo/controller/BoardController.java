752dc2bbbe1fd0bb1e980da0e1f5febd7a6c48e6
unknown <gusdudco6@naver.com>
2024-04-30 12:08:52 +0900

    ✨ Read
    read

--- src/main/java/org/example/demo/controller/BoardController.java
+++ src/main/java/org/example/demo/controller/BoardController.java
@@ -54,14 +54,12 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
         BoardDTO boardDTO = boardService.readOne(bno);
         log.info(boardDTO);
         model.addAttribute("dto",boardDTO);
 
     }
-
-
 
 }
