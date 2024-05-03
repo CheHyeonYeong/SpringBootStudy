4df2c0a1cb23756148ba0e099b353b6d50b22ab3
unknown <gusdudco6@naver.com>
2024-05-03 12:52:35 +0900

    🐛 Fix Reply indexing
    fix Reply indexing

--- src/main/java/org/example/demo/dto/PageResponseDTO.java
+++ src/main/java/org/example/demo/dto/PageResponseDTO.java
@@ -3,34 +3,33 @@ package org.example.demo.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.example.demo.dto.PageRequestDTO;
 
 import java.util.List;
 
 @Getter
 @ToString
 public class PageResponseDTO<E> {
+
     private int page;
     private int size;
     private int total;
 
-    //시작 페이지 번호
+    // 시작페이지 번호
     private int start;
-
-    //끝 페이지 번호
+    // 끝페이지 번호
     private int end;
 
-    //이전페이지 여부
+    // 이전페이지 여부
     private boolean prev;
-
-    //다음 페이지 여부
+    // 다음페이지 여부
     private boolean next;
 
-    private List<E> dtoList;       //게시글
+    private List<E> dtoList;  // 게시글 내용!
 
     @Builder(builderMethodName = "withAll")
-    public  PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total){
-
-        if(total <0){
+    public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total) {
+        if(total <= 0) {
             return;
         }
         this.page = pageRequestDTO.getPage();
@@ -39,12 +38,14 @@ public class PageResponseDTO<E> {
 
         this.dtoList = dtoList;
 
-        this.end  = (int)(Math.ceil(this.page/10.0))*10;        //화면에 표시할 페이지 번호 개수
-        this.start  = this.end -9;      //화면에서 시작번호
-        int last = (int)(Math.ceil(total/(double)size));        //데이터 개수로 계산한 마지막 페이지 번호
-        this.end = end > last ? last:end;
+        this.end = (int)(Math.ceil(this.page / 10.0)) * 10; // 화면에 표시할 페이지번호 갯수...
+        this.start = this.end - 9; // 화면에서 시작번호
+        int last = (int)(Math.ceil((total/(double)size))); // 데이터 개수로 계산한 마지막 페이지 번호
+
+        this.end = end > last ? last : end;
 
         this.prev = this.start > 1;
-        this.next = total > this.end*this.size;
+        this.next = total > this.end * this.size;
     }
+
 }
--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,27 +1,27 @@
-async function get1(bno){       //async -> 비동기 처리할 함수 명시
-    //test를 위해 만듦
-    const result = await axios.get(`/replies/list/${bno}`);      //await -> 비동기 호출
-    // console.log(result);
+async function get1(bno) {   // async ->  비동기 처리함수 명시...
+    const result = await axios.get(`/replies/list/${bno}`)   // await  -> 비동기 호출
+    // console.log(result)
+    // console.log(result.data)
     return result.data;
 }
+
 async function getList({bno, page, size, goLast}){
-    //golist는 작성시 강제적으로 마지막 페이지를 주기 위해서
-    const result = await axios.get(`/replies/list/${bno}`, {params : {page, size}});
-
-    if(goLast){
-        const total = result.data.total;
+    // console.log(bno, page, size)
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+    // console.log(result)
+    if(goLast) {
+        const total = result.data.total
         const lastPage = parseInt(Math.ceil(total/size))
-        return getList({bno:bno, page:lastPage, size:size}) //key: value 값
+        return getList({bno:bno, page:lastPage, size:size})
     }
-    return result.data;
 
+    return result.data
 }
 
-async  function addReply(replyObj){
+async function addReply(replyObj){
     const response = await axios.post(`/replies/`, replyObj)
     return response.data
 }
-
 async function getReply(rno) {
     const response = await axios.get(`/replies/${rno}`)
     console.log(response)
@@ -29,6 +29,10 @@ async function getReply(rno) {
 }
 
 async function modifyReply(replyObj) {
-    const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
+    const response = await axios.put(`/replies/${replyObj.rno}` , replyObj)
+    return response.data
+}
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
     return response.data
 }
