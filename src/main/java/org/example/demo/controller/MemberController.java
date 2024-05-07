021de4931581d33a6786d35b99f8b8aa5a4e7b7e
unknown <gusdudco6@naver.com>
2024-05-07 17:13:14 +0900

    ✨ Join
    join

--- src/main/java/org/example/demo/controller/MemberController.java
+++ src/main/java/org/example/demo/controller/MemberController.java
@@ -3,16 +3,20 @@ package org.example.demo.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.example.demo.dto.MemberJoinDTO;
+import org.example.demo.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String error, String logout){
@@ -30,10 +34,19 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinPOST(MemberJoinDTO memberJoinDTO){
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes){
         log.info("join get...............");
         log.info("memberJoinDTO : " + memberJoinDTO);
+        try{
+            memberService.join(memberJoinDTO);
 
-        return "redirect:/board/list";
+        }catch (MemberService.MidExistException e){
+            log.info(e.getMessage());
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login";        //회원 가입 후 로그인 처리
     }
 }
--- src/main/java/org/example/demo/service/MemberServiceImpl.java
+++ src/main/java/org/example/demo/service/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package org.example.demo.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.example.demo.domain.Member;
+import org.example.demo.domain.MemberRole;
 import org.example.demo.dto.MemberJoinDTO;
 import org.example.demo.repository.MemberRepository;
 import org.modelmapper.ModelMapper;
@@ -25,6 +27,16 @@ public class MemberServiceImpl implements MemberService {
         if (exist) {
             throw new MidExistException();
         }
-        
+        //modelMapper로 memberJoinDTO에 있는 값을 member 클래스로 변환
+        Member member = modelMapper.map(memberJoinDTO, Member.class);
+        member.chagePassword(passwordEncoder.encode(memberJoinDTO.getMpw()));       //password는 암호화
+        member.addRole(MemberRole.USER);        //권한부여
+
+        //결과 확인
+        log.info("=================");
+        log.info(member);
+        log.info(member.getRoleSet());
+        //db 저장
+        memberRepository.save(member);
     }
 }
